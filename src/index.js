648a87c9bb8080f7b9d28aea674062176659e0bc
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-16 14:31:03 -0400

    Separated routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import routes from './routes'
+
+const app = express();
 
 // Using body-parser to parse the request body
 app.use(bodyParser.json())
@@ -12,28 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('combined'));
 app.use(cors());
 
-const app = express();
-
-app.get('/api/resource', (req, resp) => {
-  return resp.send("Received a GET request for a resource");
-});
-
-app.post('/api/resource', (req, resp) => {
-return resp.send("Received a POST request to a resource");
-});
-
-app.get('/api/resource/:id', (req, resp) => {
-return resp.send("Received a GET request for a specified resource item");
-});
-
-app.put('/api/resource/:id', (req, resp) => {
-return resp.send("Received a PUT request for a specified resource item");
-});
-
-app.delete('/api/resource/:id', (req, resp) => {
-return resp.send("Received a DELETE request for a specified resource item");
-});
+app.use('/api', routes);
 
 app.listen(process.env.PORT, () => {
-  console.log('server listening on port 3000')
-})
+  console.log(`server listening on port ${process.env.PORT}`);
+});
