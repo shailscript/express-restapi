7cc1b02d75029260a2bfdb7e9dce2f87c4626a21
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-16 13:35:31 -0400

    Added body-parser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import bodyParser from 'body-parser'
+
+// Using body-parser to parse the request body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Using morgan for app logs
 app.use(morgan('combined'));
