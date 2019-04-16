36cecd1f90bcb2bdfa2888c20c313fd0ab81e595
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-16 12:31:27 -0400

    Morphed requests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import 'dotenv/config'
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 // Using morgan for app logs
-app.use(morgan('combined'))
-app.use(cors())
+app.use(morgan('combined'));
+app.use(cors());
 
-const app = express()
+const app = express();
 
-app.get('/', (req, resp) => {
-  return resp.send("Received a get request")
-})
+app.get('/api/resource', (req, resp) => {
+  return resp.send("Received a GET request for a resource");
+});
+
+app.post('/api/resource', (req, resp) => {
+return resp.send("Received a POST request to a resource");
+});
+
+app.get('/api/resource/:id', (req, resp) => {
+return resp.send("Received a GET request for a specified resource item");
+});
+
+app.put('/api/resource/:id', (req, resp) => {
+return resp.send("Received a PUT request for a specified resource item");
+});
+
+app.delete('/api/resource/:id', (req, resp) => {
+return resp.send("Received a DELETE request for a specified resource item");
+});
 
 app.listen(process.env.PORT, () => {
   console.log('server listening on port 3000')
