ab42a7df9329eccac4cc2b445b6faa15894ad2ec
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-17 02:11:00 -0400

    Fixed asynchronous calls

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -1,11 +1,8 @@
 import classModel from '../models/class'
-const SQL = require('sql-template-strings')
-const getDb = require('../db').getDb
 
 export default {
-    list: (req, res) => {
-        const db = getDb();
-        const classes = classModel.findAll(db);
+    list: async (req, res) => {
+        const classes = await classModel.findAll();
         res.status(200).json( {
             classes
         })
--- src/models/class.js
+++ src/models/class.js
@@ -1,6 +1,11 @@
+const SQL = require('sql-template-strings')
+const getDb = require('../db').getDb
+
 export default {
-    findAll: db=> {
-        const classes = db.all('SELECT * FROM Classes');
+    findAll: async ()=> {
+        const db = getDb();
+        const classes = await db.all('SELECT * FROM Classes');
+        console.log('I was called to give ', classes);
         return classes;
     }
 }
