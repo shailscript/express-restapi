ba82e66d5b5e3c2db438077c20fe47293c3cf7fb
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-17 02:41:12 -0400

    Exception handling and code clean

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -1,8 +1,30 @@
 import classModel from '../models/class'
 
 export default {
-    list: async (req, res) => {
-        const classes = await classModel.findAll();
+    list: async (req, res, next) => {
+        try {
+            const classes = await classModel.findAll(req, next);
+            res.status(200).json( {
+                classes: classes
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
+    
+    show: async (req, res, next) => {
+        try {
+            const classId = req.params.id * 1; //Shorthand to convert into number
+            const resultClass = await classModel.findById(classId, next);
+            if (resultClass !== undefined) {
+                return res.status(200).send(resultClass);
+            } else {
+                return res.status(404).end()
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
         res.status(200).json( {
             classes
         })
--- src/models/class.js
+++ src/models/class.js
@@ -2,16 +2,23 @@ const SQL = require('sql-template-strings')
 const getDb = require('../db').getDb
 
 export default {
-    findAll: async () => {
-        const db = getDb();
-        const classes = await db.all('SELECT * FROM Classes');
-        return classes;
+    findAll: async (req, next) => {
+        try {
+            const db = getDb();
+            const classes = await db.all('SELECT * FROM Classes');
+            return classes;
+        } catch (error) {
+            next(error);
+        }
     },
 
-    findById: async (id) => {
-        const db = getDb();
-        const resultClass = await db.all(`SELECT * FROM Classes WHERE id = ${id}`);
-        console.log('I was called to give ', resultClass);
-        return resultClass;
+    findById: async (id, next) => {
+        try {
+            const db = getDb();
+            const resultClass = await db.all(`SELECT * FROM Classes WHERE id = ${id}`);
+            return resultClass;
+        } catch (error) {
+            next(error)
+        }
     }
 }
