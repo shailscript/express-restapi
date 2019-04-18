8b30e6294f97a92fcfba1639898b55c3f3b2b0e5
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-18 00:31:22 -0400

    Teacher routes registered

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import classes from './classes'
 import students from './students'
+import teachers from './teachers'
 const router = express.Router()
 
 router.use('/classes', classes)
 router.use('/students', students)
+router.use('/teachers', teachers)
 
 export default router
