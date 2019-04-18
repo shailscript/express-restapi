903fd8f3ea234c2516b4283454bd56d3b4ab0c41
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-18 12:53:26 -0400

    Enrollment routes  registered

--- src/routes/classes/index.js
+++ src/routes/classes/index.js
@@ -11,4 +11,12 @@ classesRoutes.put('/:id', classController.update);
 
 classesRoutes.delete('/:id', classController.delete);
 
+//additonal routes
+
+classesRoutes.get('/:id/students', classController.showEnrollments);
+
+classesRoutes.post('/:id/students', classController.createEnrollment);
+
+classesRoutes.delete('/:classId/students/:studentId', classController.deleteEnrollment);
+
 export default classesRoutes
